5bf9cad0b624825f7c19ced7bbf1ce2d726a82b7
Gergo4222 <gergo.szabados11@gmail.com>
2020-07-14 16:54:42 +0200

    a
    a

---
+++ dssmegoldas/dssmegoldas/Data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dssmegoldas
+{
+    public class Data
+    {
+        public readonly string id;
+        public readonly string product;
+        public readonly int quantity;
+        public readonly DateTime dueTime;
+        public readonly int profit;
+        public readonly int penaltyForDelay;
+
+
+        public Data(string[] csvRowSplit)
+        {
+            id = csvRowSplit[0];
+            product = csvRowSplit[1];
+            
+            if (!int.TryParse(csvRowSplit[2], out quantity))
+                return;
+
+            if (!DateTime.TryParse(csvRowSplit[3], out dueTime))
+                return;
+
+            if (!int.TryParse(csvRowSplit[4], out profit))
+                return;
+
+            if (!int.TryParse(csvRowSplit[5], out penaltyForDelay))
+                return;
+        }
+    }
+}
---
+++ dssmegoldas/dssmegoldas/Methods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dssmegoldas
+{
+    public static class Methods
+    {
+        public static double CalculatePriority(this Data data, DateTime startDate)
+        {
+            double prio = (data.dueTime - startDate).TotalMinutes * (1d / data.penaltyForDelay);
+
+            return prio;
+        }
+
+        public static Data[] SortByPriority(Data[] unsortedData, DateTime startDate)
+        {
+            return unsortedData.OrderBy(x => x.CalculatePriority(startDate)).ToArray();
+        }
+    }
+}
